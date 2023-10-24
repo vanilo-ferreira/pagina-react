ecaeeaf1c9c9eaa02bdcb92c1648b8af6d9229ee
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-10-24 20:55:02 -0300

    feat: Adicionar estrututa da página React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React JS</p>
       </header>
+      <main>
+        <section className="box-titulo">
+          <h1>React JS</h1>
+          <p>Biblioteca JavaScript para criar SPAs</p>
+
+          <a href='https://reactjs.org/' target="_blank" rel="noreferrer">Site Oficial</a>
+        </section>
+        <section className="box-requisitos">
+          <h2>Requisitos</h2>
+          <p>Veja abaixo os requisitos mínimos para aprender React JS:</p>
+
+          <div className="box-cards">
+            <div className="card">
+              <h3>HTML</h3>
+              <p>Aprender a estruturar os elementos da página com HTML</p>
+            </div>
+            <div className="card">
+              <h3>CSS</h3>
+              <p>Aprender a estilizar os elementos da página com CSS</p>
+            </div>
+            <div className="card">
+              <h3>JavaScript</h3>
+              <p>Aprender a linguagem utilizada pelo React JS, o JavaScript</p>
+            </div>
+          </div>
+        </section>
+        <footer>
+          <p>Curso de introdução de React JS</p>
+        </footer>
+      </main>
     </div>
   );
 }
